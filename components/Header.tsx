c3ff6300394514eac5ed57fdd7bb36765a49ed34
gabriele pedesini <gabriele.pedesini@mail.polimi.it>
2025-09-25 13:48:15 +0200

    Fixed navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,28 @@
 import Link from "next/link";
 import { ToggleTheme } from "@/components/ToggleTheme";
+import { getLang } from "@/utils/getLang";
 
-export default function Header() {
+export default async function Header() {
+    const { dict } = await getLang(); 
+
 	return (
         <header className="navbar">
             <div className="container">
                 <ul>
                     <li>
-                        <Link href="/">Home</Link>
+                        <Link href="/">{dict.navbar.home}</Link>
                     </li>
                 
                     <li>
-                        <Link href="/notes">Notes</Link>
+                        <Link href="/notes">{dict.navbar.notes}</Link>
                     </li>
                 
                     <li>
-                        <Link href="/projects">Projects</Link>
+                        <Link href="/projects">{dict.navbar.projects}</Link>
                     </li>
                 
                     <li>
-                        <Link href="/contacts">Contacts</Link>
+                        <Link href="/contacts">{dict.navbar.contacts}</Link>
                     </li>
                 </ul>
                 
